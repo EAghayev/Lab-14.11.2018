c06e6231eb2bafb0b7bc127ea0f745d5788d7081
EAghayev <41126467+EAghayev@users.noreply.github.com>
2018-11-14 23:10:54 +0400

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Book.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Book
+    {
+
+        private string _Name;
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+        }
+
+        public string Author { get; set; }
+
+        private double _Price;
+        public double Price
+        {
+            get
+            {
+                return this._Price;
+            }
+        }
+
+        private double _SalePrice;
+        public double SalePrice
+        {
+            get
+            {
+                return this._SalePrice;
+            }
+        }
+
+        public Book(string name, double price)
+        {
+            this._Name = name;
+            this._Price = price;
+        }
+
+        public void CalcSale(double percent)
+        {
+            if (percent < 0 || percent > 100)
+            {
+                Console.WriteLine("Faiz 0-100 araliginda olmalidir!");
+            }
+            else
+            {
+                this._SalePrice = this._Price - this.Price * (percent / 100);
+            }
+        }
+
+
+
+
+
+
+    }
+}
+
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //book obyektini costructorda teleb olunan deyerleri(adi,qiymeti) daxil ederek yaradiriq
+            Book book = new Book("kitab", 100);
+            byte secim;
+            string author;
+            do
+            {
+                Console.WriteLine("1.Kitabin adi nedir?");
+                Console.WriteLine("2.Kitabin yazari kimdir?");
+                Console.WriteLine("3.Kitabin yazarini qeyd et");
+                Console.WriteLine("4.Kitabin qiymetini goster");
+                Console.WriteLine("5.Kitabin endirimli qiyetini goster");
+                Console.WriteLine("6.Kitabin endirim faizini daxil et");
+                Console.WriteLine("7.Kitabin endirim faizini goster");
+
+                Console.WriteLine("0.Cix");
+                string secimStr = Console.ReadLine();
+                while (!byte.TryParse(secimStr, out secim))
+                {
+                    secimStr = Console.ReadLine();
+                }
+                switch (secim)
+                {
+                    case 1:
+                        Console.WriteLine("Kitabin adi: " + book.Name);
+                        Console.WriteLine("==========================");
+                        break;
+                    case 2:
+                        Console.WriteLine("Kitabin yazari: " + (book.Author ?? "Kitabin muellifi qeyd edilmeyib"));
+                        Console.WriteLine("==========================");
+                        break;
+                    case 3:
+                        if (book.Author != null)
+                        {
+                            Console.WriteLine("Kitabin muellifi {0} olaraq qeyd edilib.Deyisdirmek isteyirsinizmi?(y/n)", book.Author);
+                            string yn = Console.ReadLine();
+
+                            while (yn != "y" && yn != "n")
+                            {
+                                Console.Write("Seciminiz yalnizca y ve ya n ola biler!");
+                                yn = Console.ReadLine();
+                            }
+
+                            if (yn.ToLower() == "y")
+                            {
+                                Console.Write("Kitabin muellifi: ");
+                                author = Console.ReadLine();
+                                book.Author = author;
+                            }
+                            Console.WriteLine("==========================");
+                        }
+                        else
+                        {
+                            Console.Write("Kitabin yazari: ");
+                            author = Console.ReadLine();
+                            book.Author = author;
+                            Console.WriteLine("==========================");
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("Kitabin qiymeti: " + book.Price + " AZN");
+                        Console.WriteLine("==========================");
+                        break;
+                    case 5:
+                        if (book?.SalePrice == null || book.SalePrice == 0)
+                        {
+                            Console.WriteLine("Kitab endirimde deyil!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Kitabin endirim qiymeti: " + book.SalePrice + " AZN");
+                        }
+                        Console.WriteLine("==========================");
+                        break;
+                    case 6:
+                        if (book?.SalePrice == null || book.SalePrice == 0)
+                        {
+                            Console.Write("Endirim faizini daxil edin: ");
+                            string percentStr = Console.ReadLine();
+                            double percent;
+                            while (!double.TryParse(percentStr, out percent))
+                            {
+                                Console.Write("Endirim faizini duzgun daxil edin: ");
+                                percentStr = Console.ReadLine();
+                            }
+                            book.CalcSale(percent);
+                        }
+                        else
+                        {
+                            double oldSalePercent = 100 - (book.SalePrice * 100 / book.Price);
+                            Console.WriteLine("Kitab " + oldSalePercent + "%  endirimdedir.Endirim faziini yenilemek isteyirsinizmi?(y/n)");
+                            string yn = Console.ReadLine();
+                            while (yn != "y" && yn != "n")
+                            {
+                                Console.Write("Seciminiz yalnizca y ve ya n ola biler!");
+                                yn = Console.ReadLine();
+                            }
+                            if (yn.ToLower() == "y")
+                            {
+                                Console.WriteLine("==========================");
+                                string percentStr = Console.ReadLine();
+                                double percent;
+                                while (!double.TryParse(percentStr, out percent))
+                                {
+                                    Console.Write("Endirim faizini duzgun daxil edin: ");
+                                    percentStr = Console.ReadLine();
+                                }
+                                book.CalcSale(percent);
+                            }
+
+                        }
+                        break;
+                    case 7:
+                        if (book.SalePrice == null)
+                        {
+                            Console.WriteLine("Kitab endrimde deyil!");
+                        }
+                        else
+                        {
+                            double SalePercent = 100 - (book.SalePrice * 100 / book.Price);
+                            Console.WriteLine("Kitab " + SalePercent + "%  endirimdedir");
+                        }
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        Console.WriteLine("==========================");
+                        Console.WriteLine("Seciminiz duz etmediniz");
+                        Console.WriteLine("==========================");
+                        break;
+                }
+            }
+            while (secim != 0);
+        }
+    }
+}
